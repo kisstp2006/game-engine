a661e5bdcdc8a5ecea47c377fb60a8db5f66ae3c
Guillaume HEIN <guillaume.hein@epitech.eu>
2025-03-08 17:40:21 +0900

    feat(asset-back): add JSON utils header

---
+++ engine/include/json.hpp
@@ -0,0 +1,29 @@
+//// json.hpp /////////////////////////////////////////////////////////////////
+//
+//  zzzzz       zzz  zzzzzzzzzzzzz    zzzz      zzzz       zzzzzz  zzzzz
+//  zzzzzzz     zzz  zzzz                    zzzz       zzzz           zzzz
+//  zzz   zzz   zzz  zzzzzzzzzzzzz         zzzz        zzzz             zzz
+//  zzz    zzz  zzz  z                  zzzz  zzzz      zzzz           zzzz
+//  zzz         zzz  zzzzzzzzzzzzz    zzzz       zzz      zzzzzzz  zzzzz
+//
+//  Author:      Guillaume HEIN
+//  Date:        06/03/2025
+//  Description: JSON utils for the engine
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include <nlohmann/json.hpp>
+
+namespace nexo {
+
+    using json = nlohmann::json;
+
+    template <typename T>
+    concept JSONSerializable = requires(T obj, json& j) {
+        { to_json(j, obj) } -> std::same_as<void>;
+        { from_json(j, obj) } -> std::same_as<void>;
+    };
+
+} // namespace nexo
