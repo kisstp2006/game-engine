279aa283c6fc7d23a5987594df44d5ceefc2038d
Guillaume HEIN <guillaume.hein@epitech.eu>
2025-03-27 00:00:15 +0900

    fix(AssetManagerWindow): correct file header comment to reflect source file name

--- editor/src/DocumentWindows/AssetManagerWindow.cpp
+++ editor/src/DocumentWindows/AssetManagerWindow.cpp
@@ -1,4 +1,4 @@
-//// AssetManagerWindow.hpp ///////////////////////////////////////////////////
+//// AssetManagerWindow.cpp ///////////////////////////////////////////////////
 //
 //  zzzzz       zzz  zzzzzzzzzzzzz    zzzz      zzzz       zzzzzz  zzzzz
 //  zzzzzzz     zzz  zzzz                    zzzz       zzzz           zzzz
